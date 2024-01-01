143bff085e0cb02a31f6228b35b7895956fd76af
Mohammadreza Jafari <mohammadrezajafari.dev@gmail.com>
2024-01-01 21:44:13 +0330

    feat: add config package and application config strcuts

--- src/share/config/types.go
+++ src/share/config/types.go
@@ -1,3 +1,66 @@
 package config
 
-type Config struct{}
+import "time"
+
+type Config struct {
+	Server   ServerConfig
+	Mongo MongoConfig
+	Redis    RedisConfig
+	Password PasswordConfig
+	Cors     CorsConfig
+	Logger   LoggerConfig
+	Otp      OtpConfig
+	JWT      JWTConfig
+}
+
+type ServerConfig struct {
+	InternalPort string
+	ExternalPort string
+	RunMode      string
+	RequestLimit string
+}
+
+type LoggerConfig struct {
+	FilePath string
+	Encoding string
+	Level    string
+}
+
+type MongoConfig struct {
+	Host            string
+	Port            string
+	User            string
+	Password        string
+	DbName          string
+	SSLMode         string
+	MaxIdleConns    int
+	MaxOpenConns    int
+	ConnMaxLifetime time.Duration
+}
+
+type RedisConfig struct {
+	Host               string
+	Port               string
+	Password           string
+	Db                 int
+	DialTimeout        time.Duration
+	ReadTimeout        time.Duration
+	WriteTimeout       time.Duration
+	IdleCheckFrequency time.Duration
+	PoolSize           int
+	PoolTimeout        time.Duration
+}
+
+
+
+type CorsConfig struct {
+	AllowOrigins string
+}
+
+
+type JWTConfig struct {
+	AccessTokenExpireDuration  time.Duration
+	RefreshTokenExpireDuration time.Duration
+	Secret                     string
+	RefreshSecret              string
+}
