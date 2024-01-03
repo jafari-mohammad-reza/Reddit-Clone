cf9fc2e0b523fccd9a527ad4d0c5b3cdc8289fae
Mohammadreza Jafari <mohammadrezajafari.dev@gmail.com>
2024-01-03 20:24:06 +0330

    feat: add refactored sql query for postgres database.

--- main.go
+++ main.go
@@ -4,7 +4,6 @@ import (
 	"github.com/reddit-clone/src/api"
 	"github.com/reddit-clone/src/share/config"
 	"github.com/reddit-clone/src/share/database/cache"
-	"github.com/reddit-clone/src/share/database/db"
 	"github.com/reddit-clone/src/share/pkg/custome_logger"
 )
 
@@ -17,11 +16,6 @@ func main() {
 	if err != nil {
 		lg.Error(custome_logger.Redis, custome_logger.Connect, err.Error(), nil)
 	}
-	mongoCancel, err := db.InitMongo(cfg)
-	if err != nil {
-		lg.Error(custome_logger.Mongo, custome_logger.Connect, err.Error(), nil)
-	}
-	defer mongoCancel()
 	api.InitServer(cfg)
 	lg.Info(custome_logger.General, custome_logger.Startup, "Application started", nil)
 
---
+++ src/share/database/db/postgres/pg.go
@@ -0,0 +1,10 @@
+package postgres
+
+import (
+	"context"
+
+	"github.com/reddit-clone/src/share/config"
+)
+func InitPostgres(cfg *config.Config) (context.CancelCauseFunc , error) {
+	return nil , nil
+}
