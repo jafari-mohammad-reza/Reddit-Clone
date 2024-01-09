342a4415a3be09d2fe2ff21e820409ba201cf5dd
payamafshari <pymafshari444@gmail.com>
2024-01-09 05:25:48 -0800

    import

--- src/app.go
+++ src/app.go
@@ -1,9 +1,9 @@
 package src
 
 import (
-	"Reddit-Clone/src/share/database/cache"
-	"Reddit-Clone/src/share/database/db/postgres"
-	"Reddit-Clone/src/share/database/db/postgres/migrations"
+	//"Reddit-Clone/src/share/database/cache"
+	//"Reddit-Clone/src/share/database/db/postgres"
+	//"Reddit-Clone/src/share/database/db/postgres/migrations"
 	//"github.com/reddit-clone/src/api"
 	"Reddit-Clone/src/api"
 	userdomain "Reddit-Clone/src/domains/user-domain"
@@ -21,29 +21,29 @@ type AppModule struct {
 }
 
 func InitApp(cfg *config.Config, lg custome_logger.Logger) {
-	initDb(cfg, lg)
-	queue.InitRabbitMq(cfg, lg)
-	defer queue.CloseRabbitConnection(lg)
-	defer queue.CloseRabbitChanel(lg)
+	//initDb(cfg, lg)
+	//queue.InitRabbitMq(cfg, lg)
+	//defer queue.CloseRabbitConnection(lg)
+	//defer queue.CloseRabbitChanel(lg)
 	api.InitServer(cfg)
 	userdomain.NewUserDomain()
 }
 
-func initDb(cfg *config.Config, lg custome_logger.Logger) {
-	redisCancel, err := cache.InitRedis(cfg, lg)
-	defer redisCancel()
-	if err != nil {
-		lg.Error(custome_logger.Redis, custome_logger.Connect, err.Error(), nil)
-		panic(err)
-	}
-	err = postgres.InitPostgres(cfg, lg)
-	if err != nil {
-		lg.Error(custome_logger.Postgres, custome_logger.Connect, err.Error(), nil)
-		panic(err)
-	}
-	err = migrations.SeedData()
-	if err != nil {
-		lg.Error(custome_logger.Postgres, custome_logger.Seed, err.Error(), nil)
-		panic(err)
-	}
-}
+//func initDb(cfg *config.Config, lg custome_logger.Logger) {
+//	redisCancel, err := cache.InitRedis(cfg, lg)
+//	defer redisCancel()
+//	if err != nil {
+//		lg.Error(custome_logger.Redis, custome_logger.Connect, err.Error(), nil)
+//		panic(err)
+//	}
+//	err = postgres.InitPostgres(cfg, lg)
+//	if err != nil {
+//		lg.Error(custome_logger.Postgres, custome_logger.Connect, err.Error(), nil)
+//		panic(err)
+//	}
+//	err = migrations.SeedData()
+//	if err != nil {
+//		lg.Error(custome_logger.Postgres, custome_logger.Seed, err.Error(), nil)
+//		panic(err)
+//	}
+//}
